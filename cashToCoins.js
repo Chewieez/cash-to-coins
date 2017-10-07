ae02fc16cc9983afd0b2960f976aba054f07cfe2
Greg Lawrence <gregaudio@gmail.com>
2017-10-07 14:22:12 -0500

    commented my code

--- cashToCoins.js
+++ cashToCoins.js
@@ -6,17 +6,24 @@ const piggyBank = {
     pennies: 0
 };
 
+// multiplies by 100 to remove the decimal point
 let dollarAmountMult = dollarAmount * 100;
+// converts number to a string then splits it into an array
 let dollarAmountArray = dollarAmountMult.toString().split("");
+// assigns the length of the array to a variable
 let arrayLength = dollarAmountArray.length;
 
+/* counts backwards from the back of the array and assigns each digit 
+to a unit of money in my piggyBank object. This method doesn't really 
+work for nickels so I commented that out. 
+ */
 piggyBank.pennies = parseInt(dollarAmountArray[arrayLength-1]);
 // piggyBank.nickels = parseInt(dollarAmountArray[arrayLength-1]);
 piggyBank.dimes = parseInt(dollarAmountArray[arrayLength-2]);
 piggyBank.quarters = parseInt(dollarAmountArray[arrayLength-3] / .25);
 
-
-
-
-
 console.log(piggyBank)
+
+/* I realize this isn't the best solution but it was my first working solution. 
+ I spoke with Jared and learned of a few better methods that used if/else and math. 
+ */
